0258dfa3ec5502190a9dbc7eeec9dfa3154ee40f
mustii <mustii@mmk.one>
2018-04-11 16:25:14 +0300

    Reverted to the timestamp fetching

--- db/document_datastore.rb
+++ db/document_datastore.rb
@@ -5,24 +5,25 @@ require_relative 'document_entity'
 class DocumentDatastore
 
   DOCUMENT_KIND = {'DEV' => 'page_dev', 'PROD' => 'page'}
-  LIMIT = 10
+  LIMIT = 100
   def initialize(env)
     @@datastore ||= Google::Cloud::Datastore.new(project_id: 'codegust')
-    @env = env
+    @env = envs
     # TODO remove this check in the future
     raise NotImplementedError if @env == 'PROD'
     @document_kind = DOCUMENT_KIND[@env]
   end
 
   def each_document
-    offset = 0
+    largest_timestamp = Time.now.to_i
     while true do
       query = @@datastore.query(@document_kind)
-                         .offset(offset)
+                         .where('timestamp', '<', largest_timestamp)
+                         .order('timestamp', :desc)
                          .limit(LIMIT)
 
-      offset += LIMIT
       @@datastore.run(query).each do |doc|
+        largest_timestamp = doc['timestamp']
         yield DocumentEntity.new(doc['page_url'], doc['page_title'],
                                  doc['page_scores'], doc['page_html'])
       end .empty? and begin
