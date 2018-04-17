7e6b3cbda3a0bb1935c8690f2f52e6cbdf70a468
evis <evis6174@gmail.com>
2018-04-17 20:04:10 +0300

    Delete pmap.rb

--- pmap.rb
+++
@@ -1,8 +0,0 @@
-require 'celluloid'
-# from Celluloid since it's not working normally
-module Enumerable
-    def pmap(&block)
-      futures = map { |elem| Celluloid::Future.new(elem, &block) }
-      futures.map { |future| future.value }
-    end
-  end
