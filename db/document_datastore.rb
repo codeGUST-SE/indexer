d2f25c6d15cfec31a6d428b809edf3632f16a568
evis <evis6174@gmail.com>
2018-04-20 18:39:22 +0300

    Split an index over multiple ones (#25)
    * Split indexes over multiple ones of size 40000 each
    
    * Added a cache to optimize finding the offset
    
    * Small fix
    
    * Added logs to DocumentDatastore
    
    * Small fix

--- db/document_datastore.rb
+++ db/document_datastore.rb
@@ -1,8 +1,11 @@
 require 'google/cloud/datastore'
 require_relative 'document_entity'
+require_relative '../log/log'
 
 # DocumentDatastore connects to the Datastore in Google Cloud Platform.
 class DocumentDatastore
+
+  MAX_URL_LIST = 3000
 
   DOCUMENT_KIND = {'DEV' => 'page_dev', 'PROD' => 'page'}
   INDEX_KIND = {'DEV' => 'index_dev', 'PROD' => 'index'}
@@ -15,9 +18,12 @@ class DocumentDatastore
     @document_kind = DOCUMENT_KIND[@env]
     @index_kind = INDEX_KIND[@env]
     @largest_timestamp = Time.now.to_i
+    @offset_cache = {}  # TODO not threadsafe :(
+    Log::LOGGER.info('datastore') { "Initialized with largest_timestamp = #{@largest_timestamp}" }
   end
 
   def query(limit)
+    Log::LOGGER.info('datastore') { "Query with largest_timestamp = #{@largest_timestamp}" }
     documents = []
     query = @@datastore.query(@document_kind)
                          .where('timestamp', '<', @largest_timestamp)
@@ -34,26 +40,73 @@ class DocumentDatastore
 
   def add_indexes(index_hash)
     index_hash.each_key do |index|
-      # get the current entity if it exists
-      entity_key = @@datastore.key @index_kind, index
-      entity = @@datastore.find(entity_key)
-      current_hash = entity == nil ? {} : eval(entity['value'])
+      Log::LOGGER.info('datastore') { "Adding index = #{index}" }
 
-      # create new entity or update the existing one
-      new_entity = @@datastore.entity @index_kind, index do |t|
-        t['value'] = compute_index_value(current_hash, index_hash[index])
-        t.exclude_from_indexes! 'value', true
+      current_hash, offset = get_current_hash(index)
+      new_index_value, remaining_index_value = compute_index_value(current_hash, index_hash[index])
+
+      save(index + offset.to_s, new_index_value)
+      if remaining_index_value != nil
+        save(index + (offset + 1).to_s, remaining_index_value)
+        @offset_cache[index] = offset + 1
+      else
+        @offset_cache[index] = offset
       end
-      @@datastore.save new_entity
     end
   end
 
   private
 
+  def save(index, value)
+    # create new entity or update the existing one
+    new_entity = @@datastore.entity @index_kind, index do |t|
+      t['value'] = value
+      t.exclude_from_indexes! 'value', true
+    end
+    @@datastore.save new_entity
+  end
+
+  def get_current_hash(index)
+    if @offset_cache.key?(index)
+      offset = @offset_cache[index]
+    else
+      offset = 0
+      while true
+        entity_key = @@datastore.key @index_kind, "#{index}#{offset}"
+        entity = @@datastore.find(entity_key)
+        break if entity == nil
+        offset += 1
+      end
+      offset -= 1 if offset > 0
+    end
+    entity_key = @@datastore.key @index_kind, "#{index}#{offset}"
+    entity = @@datastore.find(entity_key)
+
+    current_hash = entity == nil ? {} : eval(entity['value'])
+    return current_hash, offset
+  end
+
   # Computes the new index value given the current_hash and the new_hash.
-  # TODO: Implement top k pruning
   def compute_index_value(current_hash, new_hash)
-    current_hash.merge(new_hash).to_s
+    curr_size = current_hash.size
+    new_size = new_hash.size
+
+    if curr_size + new_size > MAX_URL_LIST
+      result_hash = current_hash
+      remaining_hash = {}
+      i = curr_size
+      new_hash.each do |key, value|
+        if i < MAX_URL_LIST
+          result_hash[key] = value
+        else
+          remaining_hash[key] = value
+        end
+        i += 1
+      end
+      return result_hash.to_s, remaining_hash.to_s
+    else
+      return current_hash.merge(new_hash).to_s, nil
+    end
   end
 
 end
